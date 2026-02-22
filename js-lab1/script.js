5994e663543d81a8aaff901e17ff61a827f2bbdc
IvanenkoNastya <ivanenko.nastya.a@gmail.com>
2026-02-23 00:26:18 +0200

    JS Lab 1  ready

---
+++ js-lab1/script.js
@@ -0,0 +1,19 @@
+let isHello = false;
+
+
+function mouseLeftMyButton() {
+    let elemToChange = document.getElementsByTagName('div')[0];
+    console.error(elemToChange.innerHTML + ' Anastasiia Ivanenko');
+}
+
+function clickOnMyButton() {
+    let elemToChange = document.getElementsByClassName('label')[0];
+    if (isHello === true) {
+        elemToChange.innerHTML = 'Goodbye world!';
+        isHello = false;
+    } else {
+        elemToChange.innerHTML = 'Hello world!';
+        isHello = true;
+    }
+    //alert('Hello World');
+}
