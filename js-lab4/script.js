e7dbcf2e2e1fea5f14c63aaa0271f31bdf98dbce
IvanenkoNastya <ivanenko.nastya.a@gmail.com>
2026-03-14 10:56:52 +0200

    started with js-lab4

---
+++ js-lab4/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// general
+
+function printMessage(message) {
+    console.log(message);
+}
+
+// task 1
+function task1() {
+    printMessage(0.1.toFixed(20));
+}
+
+// task 2
+
+// task 3
+
+// task 4
+
+// task 5
+
+// task 6
+
+// task 7
